d1f7683817bd12a0d024cf7a7223f0b1878f9d58
zty012 <me@zty012.de>
2024-09-01 14:06:02 +0800

    :bulb: 修改日志

--- src/project_graph/logging.py
+++ src/project_graph/logging.py
@@ -1,8 +1,16 @@
+import datetime
+
+
 logs: list[str] = []
 
 
 def log(*args):
     global logs
-    msg = " ".join(str(a) for a in args)
+    msg = (
+        "["
+        + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        + "] "
+        + " ".join(str(a) for a in args)
+    )
     logs.append(msg)
-    print(msg, logs)
+    print(msg)
