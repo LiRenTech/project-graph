4a962a67d5835e13a27f7f39f44b70fdd09fa830
littlefean <2028140990@qq.com>
2024-09-02 15:33:32 +0800

    refactor: 将物理设置面板移动到外部

--- src/project_graph/__main__.py
+++ src/project_graph/__main__.py
@@ -22,7 +22,6 @@ from PyQt5.QtGui import (
 from PyQt5.QtWidgets import (
     QAction,
     QApplication,
-    QCheckBox,
     QColorDialog,
     QDesktopWidget,
     QDialog,
@@ -32,7 +31,6 @@ from PyQt5.QtWidgets import (
     QMainWindow,
     QMessageBox,
     QPushButton,
-    QSlider,
     QVBoxLayout,
 )
 
@@ -62,6 +60,7 @@ from project_graph.toolbar.toolbar import Toolbar
 from project_graph.tools.file_tools import read_file
 from project_graph.ui.panel_about import show_about_panel
 from project_graph.ui.panel_help import show_help_panel
+from project_graph.ui.panel_physics_settings import show_physics_settings
 from project_graph.ui.panel_visual_settings import show_visual_settings
 
 # 导入资源文件
@@ -253,7 +252,7 @@ class Canvas(QMainWindow):
         show_settings.triggered.connect(show_visual_settings)
 
         physics_settings = QAction("物理设置", self)
-        physics_settings.triggered.connect(self.on_physics_settings)
+        physics_settings.triggered.connect(show_physics_settings)
 
         save_settings = QAction("将设置保存", self)
         save_settings.triggered.connect(SETTING_SERVICE.save_settings)
@@ -380,76 +379,6 @@ class Canvas(QMainWindow):
     def reset_scale(self):
         """重置缩放"""
         self.camera.target_scale = 1.0
-
-    def on_physics_settings(self):
-        """打开物理设置"""
-        dialog = QDialog(self)
-        dialog.setWindowTitle("物理设置")
-        dialog.setMinimumWidth(500)
-
-        # 设置布局
-        layout = QVBoxLayout()
-
-        # 开启节点碰撞
-        enable_node_collision_check_box = QCheckBox("开启节点碰撞")
-        enable_node_collision_check_box.setChecked(
-            SETTING_SERVICE.is_enable_node_collision
-        )
-
-        def on_change_enable_node_collision(state):
-            SETTING_SERVICE.is_enable_node_collision = state == 2
-
-        enable_node_collision_check_box.stateChanged.connect(
-            on_change_enable_node_collision
-        )
-        layout.addWidget(enable_node_collision_check_box)
-
-        # 镜头缩放速度滑动框 数值类型
-        layout.addWidget(QLabel("镜头缩放速度"))
-        camera_zoom_speed_slider = QSlider(Qt.Orientation.Horizontal)
-        camera_zoom_speed_slider.setMinimum(10)
-        camera_zoom_speed_slider.setMaximum(20)
-        camera_zoom_speed_slider.setValue(
-            int(SETTING_SERVICE.camera_scale_exponent * 10)
-        )
-
-        def on_change_camera_zoom_speed(value):
-            SETTING_SERVICE.camera_scale_exponent = value / 10
-
-        camera_zoom_speed_slider.valueChanged.connect(on_change_camera_zoom_speed)
-        layout.addWidget(camera_zoom_speed_slider)
-
-        # 镜头移动速度滑动框
-        layout.addWidget(QLabel("镜头移动速度"))
-        camera_move_speed_slider = QSlider(Qt.Orientation.Horizontal)
-        camera_move_speed_slider.setMinimum(1)
-        camera_move_speed_slider.setMaximum(10)
-        camera_move_speed_slider.setValue(SETTING_SERVICE.camera_move_amplitude)
-
-        def on_change_camera_move_speed(value):
-            SETTING_SERVICE.camera_move_amplitude = value
-
-        camera_move_speed_slider.valueChanged.connect(on_change_camera_move_speed)
-        layout.addWidget(camera_move_speed_slider)
-        # 镜头移动摩擦力系数
-
-        layout.addWidget(QLabel("镜头移动摩擦力系数"))
-        camera_move_friction_slider = QSlider(Qt.Orientation.Horizontal)
-        camera_move_friction_slider.setMinimum(0)
-        camera_move_friction_slider.setMaximum(10)
-        camera_move_friction_slider.setValue(
-            int(SETTING_SERVICE.camera_move_friction * 10)
-        )
-
-        def on_change_camera_move_friction(value):
-            SETTING_SERVICE.camera_move_friction = value / 10
-
-        camera_move_friction_slider.valueChanged.connect(on_change_camera_move_friction)
-        layout.addWidget(camera_move_friction_slider)
-
-        # 设置布局到对话框
-        dialog.setLayout(layout)
-        dialog.exec_()
 
     def on_test_exception(self):
         raise Exception("测试异常")
