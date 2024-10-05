a0986ec51a3e9b3852f1a91cbc6d0a96bc3141fc
littlefean <2028140990@qq.com>
2024-10-05 21:10:00 +0800

    🐛 修复windows下无法运行的问题

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,27 @@ import { RecoilRoot } from "recoil";
 import { DialogProvider } from "./utils/dialog";
 import { Settings } from "./core/Settings";
 import { RecentFileManager } from "./core/RecentFileManager";
+import { platform } from "@tauri-apps/plugin-os";
 
 console.log("Hello, world!");
 
 const router = createMemoryRouter(routes);
 const Routes = () => <RouterProvider router={router} />;
 
-// 2024/10/5 发现这里await会导致整个页面无法渲染，原因未知
-Settings.init();
-RecentFileManager.init();
+
+const currentPlatform = await platform();
+console.log(currentPlatform);
+
+if (currentPlatform === "windows") {
+  await Settings.init();
+  await RecentFileManager.init();
+} else {
+  // 2024/10/5 发现这里Linux 系统下，await会导致整个页面无法渲染，原因未知
+  // Linux
+  // Android, iOS 也可能是这样的
+  Settings.init();
+  RecentFileManager.init();
+}
 
 createRoot(document.getElementById("root")!).render(
   <RecoilRoot>
