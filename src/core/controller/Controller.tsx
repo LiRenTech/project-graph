7513e55848d0ab657094e92873a39eed515e0c93
littlefean <2028140990@qq.com>
2024-10-06 11:45:24 +0800

    ♻️ 重构节点连线功能

--- src/core/controller/Controller.tsx
+++ src/core/controller/Controller.tsx
@@ -13,6 +13,7 @@ import { Line } from "../Line";
 import { LineEffect } from "../effect/concrete/LineEffect";
 import { ControllerCamera } from "./concrete/ControllerCamera";
 import { ControllerNodeRotation } from "./concrete/ControllerNodeRotation";
+import { ControllerNodeConnection } from "./concrete/ControllerNodeConnection";
 
 /**
  * 控制器，控制鼠标、键盘事件
@@ -117,6 +118,7 @@ export namespace Controller {
     canvasElement.addEventListener("touchend", touchend, false);
     ControllerCamera.init(canvasElement);
     ControllerNodeRotation.init(canvasElement);
+    ControllerNodeConnection.init(canvasElement);
   }
 
   function mousedown(event: MouseEvent) {
@@ -227,25 +229,6 @@ export namespace Controller {
       } else {
         // 连接线
         Stage.isCutting = false;
-        // [node for node in NodeManager.nodes if node.isSelected]
-        Stage.connectFromNodes = [];
-        for (const node of NodeManager.nodes) {
-          if (node.isSelected) {
-            Stage.connectFromNodes.push(node);
-          }
-        }
-        if (Stage.connectFromNodes.includes(clickedNode)) {
-          // 多重连接
-          for (const node of NodeManager.nodes) {
-            if (node.isSelected) {
-              // 特效
-            }
-          }
-        } else {
-          // 不触发多重连接
-          Stage.connectFromNodes = [clickedNode];
-          // 特效
-        }
       }
     }
     lastMoveLocation = pressWorldLocation.clone();
@@ -350,18 +333,7 @@ export namespace Controller {
           }
         }
       } else {
-        // 连接线
-        let isFindConnectToNode = false;
-        for (const node of NodeManager.nodes) {
-          if (node.rectangle.isPointInside(worldLocation)) {
-            Stage.connectToNode = node;
-            isFindConnectToNode = true;
-            break;
-          }
-        }
-        if (!isFindConnectToNode) {
-          Stage.connectToNode = null;
-        }
+        
       }
     }
     if (pressingKeySet.has("`")) {
@@ -402,52 +374,6 @@ export namespace Controller {
       }
     } else if (button === 2) {
       // 右键松开
-
-      // 结束连线
-      if (Stage.connectFromNodes.length > 0 && Stage.connectToNode !== null) {
-        let isHaveConnectResult = false; // 在多重链接的情况下，是否有连接成功
-        for (const node of Stage.connectFromNodes) {
-          const connectResult = NodeManager.connectNode(
-            node,
-            Stage.connectToNode,
-          );
-          if (connectResult) {
-            // 连接成功，特效
-            isHaveConnectResult = true;
-            Stage.effects.push(
-              new CircleFlameEffect(
-                new ProgressNumber(0, 15),
-                node.rectangle.center,
-                80,
-                new Color(83, 175, 29, 1),
-              ),
-            );
-            Stage.effects.push(
-              new LineCuttingEffect(
-                new ProgressNumber(0, 30),
-                node.rectangle.center,
-                Stage.connectToNode.rectangle.center,
-                new Color(78, 201, 176, 1),
-                new Color(83, 175, 29, 1),
-                20,
-              ),
-            );
-          }
-        }
-        if (isHaveConnectResult) {
-          // 给连向的那个节点加特效
-          Stage.effects.push(
-            new CircleFlameEffect(
-              new ProgressNumber(0, 15),
-              Stage.connectToNode.rectangle.center,
-              80,
-              new Color(0, 255, 0, 1),
-            ),
-          );
-        }
-      }
-      Stage.connectFromNodes = [];
-      Stage.connectToNode = null;
 
       if (Stage.isCutting) {
         NodeManager.deleteNodes(Stage.warningNodes);
@@ -509,7 +435,6 @@ export namespace Controller {
       // 结束切断线
       Stage.isCutting = false;
     }
-    // Stage.effects.push(new TextRiseEffect("mouse up"));
   }
 
   function mousewheel(_: WheelEvent) {
@@ -690,6 +615,7 @@ export namespace Controller {
     canvasElement?.removeEventListener("touchend", touchend);
     ControllerCamera.destroy(canvasElement!);
     ControllerNodeRotation.destroy(canvasElement!);
+    ControllerNodeConnection.destroy(canvasElement!);
     console.log("Controller destroyed.");
   }
 }
