fde38d06476d1375465153a16585e7b7923af063
littlefean <2028140990@qq.com>
2024-10-13 19:46:41 +0800

    🔍 增加一个QQ群跳转按钮

--- src/pages/settings/about.tsx
+++ src/pages/settings/about.tsx
@@ -84,6 +84,9 @@ export default function About() {
         <Button onClick={() => open("https://liren.zty012.de/project-graph")}>
           Website
         </Button>
+        <Button onClick={() => open("https://qm.qq.com/cgi-bin/qm/qr?k=1Wskf2Y2KJz3ARpCgzi04y_p95a78Wku&jump_from=webapi&authKey=EkjB+oWihwZIfyqVsIv2dGrNv7bhSGSIULM3+ZLU2R5AVxOUKaIRwi6TKOHlT04/")}>
+          QQ群
+        </Button>
       </div>
     </div>
   );
