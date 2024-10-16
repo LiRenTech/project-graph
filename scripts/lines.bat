ad8e17994aba4c26e4922d932af54bc603f168ae
littlefean <2028140990@qq.com>
2024-10-16 23:47:49 +0800

    🔨 更新脚本

--- scripts/lines.bat
+++ scripts/lines.bat
@@ -4,12 +4,15 @@ setlocal enabledelayedexpansion
 
 set totalLines=0
 
-REM 获取当前目录的上级目录
+REM 获取当前脚本文件所在的目录
+set scriptDir=%~dp0
+
+REM 获取项目目录，即脚本所在目录的上级目录
+cd /d "%scriptDir%"
 cd ..
 set projectDir=%cd%
 
 echo 正在扫描项目目录 %projectDir% 及子目录下的所有 .tsx 文件...
-
 
 REM 扫描项目目录及子目录下的所有 .tsx 文件，忽略 node_modules 目录
 for /r "%projectDir%" %%f in (*.tsx) do (
