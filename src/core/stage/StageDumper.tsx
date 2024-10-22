e11fa946c2e40e9d8bed98776b839b3893019a5c
littlefean <2028140990@qq.com>
2024-10-22 17:38:00 +0800

    fix bug

--- src/core/stage/StageDumper.tsx
+++ src/core/stage/StageDumper.tsx
@@ -1,27 +1,26 @@
 import { Serialized } from "../../types/node";
 import { Edge } from "../stageObject/association/Edge";
 import { TextNode } from "../stageObject/entity/TextNode";
+import { Entity } from "../stageObject/StageObject";
 import { StageManager } from "./stageManager/StageManager";
 
 /**
  * 将舞台信息转化为序列化JSON对象
  */
 export namespace StageDumper {
-
   /**
    * 最新版本
    */
   export const latestVersion = 7;
 
-
-  export function dumpNode(node: TextNode): Serialized.Node {
+  export function dumpTextNode(textNode: TextNode): Serialized.Node {
     return {
-      location: [node.rectangle.location.x, node.rectangle.location.y],
-      size: [node.rectangle.size.x, node.rectangle.size.y],
-      text: node.text,
-      uuid: node.uuid,
-      details: node.details,
-      color: node.color && node.color.toArray(),
+      location: [textNode.rectangle.location.x, textNode.rectangle.location.y],
+      size: [textNode.rectangle.size.x, textNode.rectangle.size.y],
+      text: textNode.text,
+      uuid: textNode.uuid,
+      details: textNode.details,
+      color: textNode.color && textNode.color.toArray(),
     };
   }
 
@@ -41,7 +40,7 @@ export namespace StageDumper {
   export function dump(): Serialized.File {
     return {
       version: latestVersion,
-      nodes: StageManager.getTextNodes().map((node) => dumpNode(node)),
+      nodes: StageManager.getTextNodes().map((node) => dumpTextNode(node)),
       edges: StageManager.getEdges().map((edge) => dumpEdge(edge)),
     };
   }
@@ -51,10 +50,12 @@ export namespace StageDumper {
    * @param nodes 选中的节点
    * @returns
    */
-  export function dumpSelected(nodes: TextNode[]): Serialized.File {
-    const selectedNodes = nodes.map((node) => dumpNode(node));
+  export function dumpSelected(nodes: Entity[]): Serialized.File {
+    const selectedNodes = nodes
+      .filter((node) => node instanceof TextNode)
+      .map((node) => dumpTextNode(node));
     const selectedEdges: Serialized.Edge[] = [];
-    
+
     for (const edge of StageManager.getEdges()) {
       if (nodes.includes(edge.source) && nodes.includes(edge.target)) {
         selectedEdges.push(dumpEdge(edge));
