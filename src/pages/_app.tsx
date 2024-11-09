e9c541236083f0104d7938123a87db6c28472cc2
zty012 <me@zty012.de>
2024-11-09 08:57:50 +0800

    🐛 修复标题栏文字无法移动窗口

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -276,6 +276,7 @@ export default function App() {
               )}
             ></Button>
             <span
+              data-tauri-drag-region
               className={cn(
                 isSaved ? "" : "text-yellow-500",
                 "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
