3faaf7d11cd7cf9737981bdc5cbe220a189f8d74
littlefean <2028140990@qq.com>
2024-11-09 23:32:46 +0800

    ✅ 添加测试样例

---
+++ src/__tests__/unit/vector.test.tsx
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+import { Vector } from "../../core/dataStruct/Vector";
+
+describe("Vector", () => {
+  it("1+1=2", () => {
+    expect(1 + 1).toEqual(2);
+  });
+  it("v1+v2", () => {
+    const v1 = new Vector(1, 2);
+    const v2 = new Vector(3, 4);
+    const v3 = v1.add(v2);
+    expect(v3.x).toEqual(4);
+    expect(v3.y).toEqual(6);
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    // 配置测试环境
+    environment: 'node',  // 环境, node 或 jsdom
+    // 全局设置
+    globals: true,
+    include: ["src/__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    // 设置覆盖率报告
+    // coverage: {
+    //   provider: 'c8',
+    //   reporter: ['text', 'json', 'html']
+    // },
+    // 其他配置选项
+  }
+});
