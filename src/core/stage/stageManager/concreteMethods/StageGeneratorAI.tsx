5d64514729e061cb40e61331830146bbbdc1d9f6
littlefean <2028140990@qq.com>
2024-11-12 14:42:27 +0800

    ✨ 初步可用

--- src/core/stage/stageManager/concreteMethods/StageGeneratorAI.tsx
+++ src/core/stage/stageManager/concreteMethods/StageGeneratorAI.tsx
@@ -1,17 +1,30 @@
 import { ArrayFunctions } from "../../../algorithm/arrayFunctions";
-import { Random } from "../../../algorithm/random";
 import { Vector } from "../../../dataStruct/Vector";
 import { EdgeRenderer } from "../../../render/canvas2d/entityRenderer/edge/EdgeRenderer";
 import { TextNode } from "../../../stageObject/entity/TextNode";
 import { Stage } from "../../Stage";
 import { StageManager } from "../StageManager";
 import { v4 as uuidv4 } from "uuid";
+import { fetch } from "@tauri-apps/plugin-http";
 
 export namespace StageGeneratorAI {
+
+  export let fetchUrl = "https://ark.cn-beijing.volces.com/api/v3/chat/completions";
+  export let authorization = "";
+  export let model = "ep-20240826150107-wkr2r";
+
   /**
    * 扩展所有选中的节点
    */
   export async function generateNewTextNodeBySelected() {
+    if (authorization === "") {
+      const authorizationStr = prompt("请先设置AI生成的authorizationString");
+      if (authorizationStr === null) {
+        return;
+      } else {
+        authorization = authorizationStr;
+      }
+    }
     const selectedTextNodes = StageManager.getSelectedEntities().filter(
       (entity) => entity instanceof TextNode,
     );
@@ -22,28 +35,74 @@ export namespace StageGeneratorAI {
     // 遍历所有选中节点
     for (const selectedTextNode of selectedTextNodes) {
       selectedTextNode.isAiGenerating = true;
-      const expandArrayList = await virtualGenerateTextList();
+      const expandArrayList = await realGenerateTextList(selectedTextNode);
       selectedTextNode.isAiGenerating = false;
       generateChildNodes(selectedTextNode, expandArrayList);
     }
   }
 
-  function virtualGenerateTextList() {
-    // 使用 Promise 来实现延迟
-    return new Promise<string[]>((resolve) => {
-      setTimeout(
-        () => {
-          const expandArrayList: string[] = [];
-          for (let i = 0; i < Random.randomInt(3, 10); i++) {
-            expandArrayList.push(
-              "expand" + "_".repeat(Random.randomInt(3, 20)),
-            );
-          }
-          resolve(expandArrayList); // 1秒后返回数组
+  // function virtualGenerateTextList() {
+  //   // 使用 Promise 来实现延迟
+  //   return new Promise<string[]>((resolve) => {
+  //     setTimeout(
+  //       () => {
+  //         const expandArrayList: string[] = [];
+  //         for (let i = 0; i < Random.randomInt(3, 10); i++) {
+  //           expandArrayList.push(
+  //             "expand" + "_".repeat(Random.randomInt(3, 20)),
+  //           );
+  //         }
+  //         resolve(expandArrayList); // 1秒后返回数组
+  //       },
+  //       Random.randomInt(500, 4000),
+  //     );
+  //   });
+  // }
+
+  async function realGenerateTextList(selectedTextNode: TextNode) {
+    try {
+      const response = await fetch(
+        fetchUrl,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: authorization,
+          },
+          body: JSON.stringify({
+            model: model,
+            messages: [
+              {
+                role: "system",
+                content:
+                  "你是一个具有发散式思维的人，我给你一个词之后，你会扩展出5~10个新的词。你需要以每行一个词的形式回答我，每个词之间用换行符分隔，不要有任何其他多余文字",
+              },
+              {
+                role: "user",
+                content: `我的词是：${selectedTextNode.text}，你会扩展出5~10个新的词。每个词之间用换行符分隔`,
+              },
+            ],
+          }),
         },
-        Random.randomInt(500, 4000),
       );
-    });
+      const data = await response.json();
+      const responseContent: string = data.choices[0].message.content;
+      let expandArrayList: string[] = []
+      if (responseContent.includes("\n")) {
+        // 多行
+        expandArrayList = responseContent
+        .split("\n")
+        .map((line) => line.trim());
+      } else {
+        // 按照顿号分割
+        expandArrayList = responseContent.split("、").map((line) => line.trim());
+      }
+      
+
+      return expandArrayList; // 返回扩展的字符串数组
+    } catch (error) {
+      return ["error"];
+    }
   }
 
   function generateChildNodes(parent: TextNode, childTextList: string[]) {
