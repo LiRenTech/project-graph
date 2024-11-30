30194622863a21b485dc8083c45e0cf32e67a4f5
littlefean <2028140990@qq.com>
2024-11-30 17:58:10 +0800

    ✨ 增加新的ai模型

---
+++ src/__tests__/unit/aiFetcher.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it } from "vitest";
+import { AiFetcherOneShotCloudFlare, AiFetcherOneShotDoubao } from "../../core/ai/AiFetcher";
+
+describe("aiFetcher", () => {
+  it("豆包", async () => {
+    const result = await AiFetcherOneShotDoubao.create()
+      .setPrompt({
+        system: "你是一个助手",
+        user: "你好",
+      })
+      .setApiKey("**************************")
+      .fetch();
+
+    console.log(result);
+  });
+
+  it("Cloudflare", async () => {
+    const result = await AiFetcherOneShotCloudFlare.create()
+      .setPrompt({
+        system: "你是一个助手",
+        user: "你好",
+      })
+      .setApiKey("**************************")
+      .fetch();
+
+    console.log(result);
+  });
+});
