d151076232952d2db5165fede830561152c0b3ad
zty012 <me@zty012.de>
2024-12-21 20:32:17 +0800

    :technologist: 优化代码结构

--- src/pages/settings/_layout.tsx
+++ src/pages/settings/_layout.tsx
@@ -19,46 +19,40 @@ export default function SettingsLayout() {
     <div className="fixed bottom-20 left-20 right-20 top-24">
       <h1 className="text-3xl font-bold">设置</h1>
       <div className="mt-4 flex h-full gap-8">
-        <div className="flex flex-col gap-2 *:cursor-pointer *:rounded-full *:px-3 *:py-2 *:transition hover:*:bg-neutral-800 active:*:scale-90 page:*:bg-white/10">
-          <NavLink to="/settings/about" className="flex items-center gap-2">
+        <div className="flex flex-col gap-2 *:flex *:cursor-pointer *:items-center *:gap-2 *:rounded-full *:px-3 *:py-2 *:transition hover:*:bg-neutral-800 active:*:scale-90 page:*:bg-white/10">
+          <NavLink to="/settings/about">
             <Info />
             {t("tabs.about")}
           </NavLink>
-          <NavLink to="/settings/visual" className="flex items-center gap-2">
+          <NavLink to="/settings/visual">
             <Eye />
             {t("tabs.visual")}
           </NavLink>
-          <NavLink
-            to="/settings/performance"
-            className="flex items-center gap-2"
-          >
+          <NavLink to="/settings/performance">
             <Sparkles />
             {t("tabs.performance")}
           </NavLink>
-          <NavLink
-            to="/settings/automation"
-            className="flex items-center gap-2"
-          >
+          <NavLink to="/settings/automation">
             <Bot />
             {t("tabs.automation")}
           </NavLink>
-          <NavLink to="/settings/control" className="flex items-center gap-2">
+          <NavLink to="/settings/control">
             <Keyboard />
             {t("tabs.control")}
           </NavLink>
-          <NavLink to="/settings/ai" className="flex items-center gap-2">
+          <NavLink to="/settings/ai">
             <Brain />
             {t("tabs.ai")}
           </NavLink>
-          <NavLink to="/settings/github" className="flex items-center gap-2">
+          <NavLink to="/settings/github">
             <Github />
             {t("tabs.github")}
           </NavLink>
-          <NavLink to="/settings/sounds" className="flex items-center gap-2">
+          <NavLink to="/settings/sounds">
             <Speaker />
             {t("tabs.sounds")}
           </NavLink>
-          <NavLink to="/settings/plugins" className="flex items-center gap-2">
+          <NavLink to="/settings/plugins">
             <Plug />
             {t("tabs.plugins")}
           </NavLink>
