a743081d99ed146a8d4553d6a21e66dc977c8cad
littlefean <2028140990@qq.com>
2025-01-20 00:29:36 +0800

    ♻️ 增加SvgRenderer

--- src/core/render/canvas2d/RenderUtils.tsx
+++ src/core/render/canvas2d/RenderUtils.tsx
@@ -174,36 +174,6 @@ export namespace RenderUtils {
     Canvas.ctx.fill();
   }
 
-  export function renderSvgFromLeftTop(
-    svg: string,
-    location: Vector,
-    width: number,
-    height: number,
-  ): void {
-    const data = svg;
-    const img = new Image();
-    img.src = "data:image/svg+xml;base64," + btoa(data);
-    Canvas.ctx.drawImage(img, location.x, location.y, width, height);
-  }
-
-  export function renderSvgFromCenter(
-    svg: string,
-    centerLocation: Vector,
-    width: number,
-    height: number,
-  ): void {
-    const data = svg;
-    const img = new Image();
-    img.src = "data:image/svg+xml;base64," + btoa(data);
-    Canvas.ctx.drawImage(
-      img,
-      centerLocation.x - width / 2,
-      centerLocation.y - height / 2,
-      width,
-      height,
-    );
-  }
-
   /**
    * 绘制一个像素点
    * @param location
