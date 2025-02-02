d385e4259bd7819f25c1bb2ef78ccd219eb3a1da
zty012 <me@zty012.de>
2025-02-02 15:25:25 +0800

    ⬆️ update vitest

--- tests/monoStack.test.tsx
+++ tests/monoStack.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect } from "vitest";
-import { MonoStack } from "../../core/dataStruct/MonoStack";
+import { describe, expect, it } from "vitest";
+import { MonoStack } from "../src/core/dataStruct/MonoStack";
 describe("monoStack", () => {
   /**
    * a
