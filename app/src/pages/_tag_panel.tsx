cfa136b03fe96ea5dcc6955de3dab967bca00564
littlefean <2028140990@qq.com>
2025-02-07 05:32:44 +0800

    💄 优化标签面板样式

--- app/src/pages/_tag_panel.tsx
+++ app/src/pages/_tag_panel.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Button from "../components/Button";
 import { Dialog } from "../components/dialog";
 import { StageManager } from "../core/stage/stageManager/StageManager";
 import { cn } from "../utils/cn";
+import IconButton from "../components/IconButton";
+import { Plus, RefreshCcw } from "lucide-react";
 
 /**
  * 标签相关面板
@@ -42,28 +43,48 @@ export default function TagPanel({ open = false, className = "" }: { open: boole
   return (
     <div
       className={cn(
-        "bg-panel-bg fixed -left-64 top-16 flex h-96 w-64 flex-col rounded-md p-2 transition-all",
+        "bg-panel-bg fixed -left-64 top-16 flex h-full w-64 flex-col rounded-md p-4 transition-all",
         {
           "left-0": open,
         },
         className,
       )}
     >
-      <div>
-        <Button onClick={handleClickAddTag}>将选中节点添加标签</Button>
-        <Button onClick={updateTagNameList}>刷新</Button>
+      <div className="flex justify-center gap-2">
+        <IconButton
+          onClick={handleClickAddTag}
+          tooltip="添加选中的文本节点成为标签，如果选中了已经是标签的节点，则会取消标签状态"
+        >
+          <Plus />
+        </IconButton>
+        <IconButton onClick={updateTagNameList} tooltip="如果舞台上的标签发生变更但此处未更新，可以手动刷新">
+          <RefreshCcw />
+        </IconButton>
       </div>
 
       {/* 标签列表 */}
-      <div className="flex-1 overflow-y-auto">
-        {tagNameList.map((tag) => {
-          return (
-            <div key={tag.uuid} className="cursor-pointer hover:bg-neutral-600" onClick={handleClickTag(tag.uuid)}>
-              {tag.tagName}
-            </div>
-          );
-        })}
-      </div>
+      {tagNameList.length === 0 ? (
+        <div>
+          <h3 className="text-select-text text-lg">当前还没有标签</h3>
+          <p className="text-select-option-hover-text text-sm">
+            给节点添加标签后会显示在左侧面板中，方便知道舞台上都有哪些主要内容，点击内容即可跳转
+          </p>
+        </div>
+      ) : (
+        <div className="mt-2 flex-1 flex-col justify-center overflow-y-auto">
+          {tagNameList.map((tag) => {
+            return (
+              <div
+                key={tag.uuid}
+                className="text-select-option-text hover:text-select-option-hover-text cursor-pointer text-center hover:underline"
+                onClick={handleClickTag(tag.uuid)}
+              >
+                {tag.tagName}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
