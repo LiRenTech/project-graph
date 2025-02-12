527f9068ff3da489ac5896462eee6abb2639cc79
littlefean <2028140990@qq.com>
2025-02-12 22:45:26 +0800

    🧪 添加cli测试

--- app/src/cli.tsx
+++ app/src/cli.tsx
@@ -4,6 +4,10 @@ import { writeTextFile } from "./utils/fs";
 import { writeStdout } from "./utils/otherApi";
 
 export async function runCli(matches: CliMatches) {
+  if (matches.args.help?.occurrences > 0) {
+    writeStdout(cliHelpText);
+    return;
+  }
   if (matches.args.output?.occurrences > 0) {
     const outputPath = matches.args.output?.value as string;
     const outputFormat = outputPath.endsWith(".svg") || outputPath === "-" ? "svg" : "";
@@ -19,3 +23,14 @@ export async function runCli(matches: CliMatches) {
     }
   }
 }
+const cliHelpText = `
+    ____               _           __  ______                 __  
+   / __ \\_________    (_)__  _____/ \\/ ____/________ _____  / /_ 
+  / /_/ / ___/ __ \\  / / _ \\/ ___/ __\\/ __/ ___/ __ \\/ __ \\/ __ \\
+ / ____/ /  / /_/ / / /  __/ /__/ /_/ \\/_/ / /  / /_/ / /_/ / / / /
+/_/   /_/   \\____/_/ /\\___/\\___/\\__\\/____/_/   \\__,_/ .___/_/ /_/ 
+                /___/                              /_/            
+
+https://project-graph.top/zh/features/cli
+
+`;
