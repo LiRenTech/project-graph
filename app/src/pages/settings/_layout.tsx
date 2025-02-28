ca86dec292a72f70f3f6e0739994459cfb89c882
littlefean <2028140990@qq.com>
2025-02-28 13:29:43 +0800

    💄 完善Settings页面布局，防止左侧有项因屏幕过小而看不到

--- app/src/pages/settings/_layout.tsx
+++ app/src/pages/settings/_layout.tsx
@@ -7,15 +7,14 @@ import { isMac } from "../../utils/platform";
 
 export default function SettingsLayout() {
   const { t } = useTranslation("settings");
-
   return (
-    <div className="fixed bottom-20 left-20 right-20 top-24">
-      <h1 className="text-appmenu-item-text flex items-center gap-2 text-3xl font-bold">
+    <div className="top-18 fixed bottom-12 left-20 right-20 flex flex-col">
+      <h1 className="text-appmenu-item-text my-2 flex items-center gap-2 text-3xl font-bold">
         <Settings className="h-8 w-8" />
         {t("title")}
       </h1>
-      <div className="mt-4 flex h-full gap-8">
-        <div className="text-appmenu-item-text *:hover:bg-appmenu-hover-bg flex flex-col gap-2 *:flex *:cursor-pointer *:items-center *:gap-2 *:rounded-full *:px-3 *:py-2 *:transition *:active:scale-90">
+      <div className="flex flex-1 gap-8 overflow-hidden">
+        <div className="text-appmenu-item-text *:hover:bg-appmenu-hover-bg flex h-full flex-col gap-2 overflow-y-auto *:flex *:cursor-pointer *:items-center *:gap-2 *:rounded-full *:px-3 *:py-2 *:transition *:active:scale-90">
           <NavLinkButton to="/settings/about">
             <Info />
             {t("tabs.about")}
@@ -62,7 +61,7 @@ export default function SettingsLayout() {
             {t("tabs.plugins")}
           </NavLinkButton>
         </div>
-        <div className="mx-auto w-[1024px] overflow-auto">
+        <div className="flex-1 overflow-auto">
           <Outlet />
         </div>
       </div>
