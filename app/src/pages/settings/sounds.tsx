5b9bb30e9dfa790acff50f0523f40ffce3a7fc0f
littlefean <2028140990@qq.com>
2025-03-09 11:51:37 +0800

    📝 增加提示

--- app/src/pages/settings/sounds.tsx
+++ app/src/pages/settings/sounds.tsx
@@ -9,6 +9,7 @@ import {
   ToggleLeft,
   ToggleRight,
 } from "lucide-react";
+import Github from "../../assets/github.svg?react";
 import { open as openFileDialog } from "@tauri-apps/plugin-dialog";
 import { SettingField } from "./_field";
 import Button from "../../components/Button";
@@ -17,95 +18,74 @@ import { PathString } from "../../utils/pathString";
 import { exists } from "../../utils/fs";
 import { Dialog } from "../../components/dialog";
 
+const SOUND_CONFIGS: { fileName: string; settingKey: keyof Settings.Settings; icon: React.ReactNode }[] = [
+  { fileName: "cuttingLineStart.mp3", settingKey: "cuttingLineStartSoundFile", icon: <Scissors /> },
+  { fileName: "cuttingLineRelease.mp3", settingKey: "cuttingLineReleaseSoundFile", icon: <ScissorsLineDashed /> },
+  { fileName: "connectLineStart.mp3", settingKey: "connectLineStartSoundFile", icon: <ArrowUpFromDot /> },
+  { fileName: "connectFindTarget.mp3", settingKey: "connectFindTargetSoundFile", icon: <ArrowDownToDot /> },
+  { fileName: "alignAndAttach.mp3", settingKey: "alignAndAttachSoundFile", icon: <AlignStartVertical /> },
+  { fileName: "uiButtonEnter.mp3", settingKey: "uiButtonEnterSoundFile", icon: <SquareMousePointer /> },
+  { fileName: "uiButtonClick.mp3", settingKey: "uiButtonClickSoundFile", icon: <MousePointerClick /> },
+  { fileName: "uiSwitchButtonOn.mp3", settingKey: "uiSwitchButtonOnSoundFile", icon: <ToggleRight /> },
+  { fileName: "uiSwitchButtonOff.mp3", settingKey: "uiSwitchButtonOffSoundFile", icon: <ToggleLeft /> },
+];
+
 export default function Sounds() {
   const handleClickImportSounds = async () => {
-    // 打开一个文件夹，识别该文件夹的路径
     const folderPath = await openFileDialog({
       title: "打开文件夹",
       directory: true,
       multiple: false,
       filters: [],
     });
-    const p = (fileName: string) => folderPath + PathString.getSep() + fileName;
+
     if (folderPath) {
-      // D:\XX\XXX\project-graph
-      // console.log(folderPath);
+      const pathBuilder = (fileName: string) => `${folderPath}${PathString.getSep()}${fileName}`;
 
-      const lostFiles = [];
-      if (await exists(p("cuttingLineStart.mp3"))) {
-        Settings.set("cuttingLineStartSoundFile", p("cuttingLineStart.mp3"));
-      } else {
-        lostFiles.push("cuttingLineStart.mp3");
-      }
+      const checkResults = await Promise.all(
+        SOUND_CONFIGS.map(async ({ fileName, settingKey }) => {
+          const fullPath = pathBuilder(fileName);
+          const fileExists = await exists(fullPath);
+          if (fileExists) {
+            Settings.set(settingKey, fullPath);
+          }
+          return { fileName, exists: fileExists };
+        }),
+      );
 
-      if (await exists(p("cuttingLineRelease.mp3"))) {
-        Settings.set("cuttingLineReleaseSoundFile", p("cuttingLineRelease.mp3"));
-      } else {
-        lostFiles.push("cuttingLineRelease.mp3");
-      }
-      if (await exists(p("connectLineStart.mp3"))) {
-        Settings.set("connectLineStartSoundFile", p("connectLineStart.mp3"));
-      } else {
-        lostFiles.push("connectLineStart.mp3");
-      }
-      if (await exists(p("connectFindTarget.mp3"))) {
-        Settings.set("connectFindTargetSoundFile", p("connectFindTarget.mp3"));
-      } else {
-        lostFiles.push("connectFindTarget.mp3");
-      }
-      if (await exists(p("alignAndAttach.mp3"))) {
-        Settings.set("alignAndAttachSoundFile", p("alignAndAttach.mp3"));
-      } else {
-        lostFiles.push("alignAndAttach.mp3");
-      }
-      if (await exists(p("uiButtonEnter.mp3"))) {
-        Settings.set("uiButtonEnterSoundFile", p("uiButtonEnter.mp3"));
-      } else {
-        lostFiles.push("uiButtonEnter.mp3");
-      }
-      if (await exists(p("uiButtonClick.mp3"))) {
-        Settings.set("uiButtonClickSoundFile", p("uiButtonClick.mp3"));
-      } else {
-        lostFiles.push("uiButtonClick.mp3");
-      }
-      if (await exists(p("uiSwitchButtonOn.mp3"))) {
-        Settings.set("uiSwitchButtonOnSoundFile", p("uiSwitchButtonOn.mp3"));
-      } else {
-        lostFiles.push("uiSwitchButtonOn.mp3");
-      }
-      if (await exists(p("uiSwitchButtonOff.mp3"))) {
-        Settings.set("uiSwitchButtonOffSoundFile", p("uiSwitchButtonOff.mp3"));
-      } else {
-        lostFiles.push("uiSwitchButtonOff.mp3");
-      }
-      if (lostFiles.length > 0) {
-        Dialog.show({
-          title: "提示",
-          content: `未找到以下${lostFiles.length}个文件：${lostFiles.join(", ")}`,
-          type: "warning",
-        });
-      } else {
-        Dialog.show({
-          title: "提示",
-          content: "导入成功",
-          type: "success",
-        });
-      }
+      const lostFiles = checkResults.filter(({ exists }) => !exists).map(({ fileName }) => fileName);
+
+      Dialog.show({
+        title: "提示",
+        content:
+          lostFiles.length > 0
+            ? `未找到以下${lostFiles.length}个文件：${lostFiles.join(", ")}`
+            : `导入成功，已更新${SOUND_CONFIGS.length}个设置`,
+        type: lostFiles.length > 0 ? "warning" : "success",
+      });
     }
   };
 
   return (
     <>
-      <SettingField icon={<Scissors />} settingKey="cuttingLineStartSoundFile" type="file" />
-      <SettingField icon={<ScissorsLineDashed />} settingKey="cuttingLineReleaseSoundFile" type="file" />
-      <SettingField icon={<ArrowUpFromDot />} settingKey="connectLineStartSoundFile" type="file" />
-      <SettingField icon={<ArrowDownToDot />} settingKey="connectFindTargetSoundFile" type="file" />
-      <SettingField icon={<AlignStartVertical />} settingKey="alignAndAttachSoundFile" type="file" />
-      <SettingField icon={<SquareMousePointer />} settingKey="uiButtonEnterSoundFile" type="file" />
-      <SettingField icon={<MousePointerClick />} settingKey="uiButtonClickSoundFile" type="file" />
-      <SettingField icon={<ToggleRight />} settingKey="uiSwitchButtonOnSoundFile" type="file" />
-      <SettingField icon={<ToggleLeft />} settingKey="uiSwitchButtonOffSoundFile" type="file" />
-      <Button onClick={handleClickImportSounds}>一键导入官方音效</Button>
+      {SOUND_CONFIGS.map(({ icon, settingKey }) => (
+        <SettingField key={settingKey} icon={icon} settingKey={settingKey} type="file" />
+      ))}
+      <div className="flex flex-nowrap gap-2">
+        <Button
+          tooltip="https://github.com/LiRenTech/project-graph/tree/master/sounds"
+          onClick={() => open("https://github.com/LiRenTech/project-graph/tree/master/sounds")}
+        >
+          <Github />
+          <span>获取官方音效文件夹</span>
+        </Button>
+        <Button
+          onClick={handleClickImportSounds}
+          tooltip="请提前下载官方音效文件夹，并点击此按钮，从本地选择官方音效文件夹"
+        >
+          从官方音效文件夹导入全部音效
+        </Button>
+      </div>
     </>
   );
 }
