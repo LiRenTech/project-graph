1c131903bbaab6b76ed89446df1fa316da2fd98c
littlefean <2028140990@qq.com>
2025-03-17 01:31:31 +0800

    📝 更新prettierignore文件

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,7 +1,7 @@
 // Generouted, changes to this file will be overridden
 /* eslint-disable */
 
-import { components, hooks, utils } from '@generouted/react-router/client'
+import { components, hooks, utils } from "@generouted/react-router/client";
 
 export type Path =
   | `/`
@@ -21,14 +21,12 @@ export type Path =
   | `/settings/visual`
   | `/test`
   | `/ui_test`
-  | `/welcome`
+  | `/welcome`;
 
-export type Params = {
-  
-}
+export type Params = {};
 
-export type ModalPath = never
+export type ModalPath = never;
 
-export const { Link, Navigate } = components<Path, Params>()
-export const { useModals, useNavigate, useParams } = hooks<Path, Params, ModalPath>()
-export const { redirect } = utils<Path, Params>()
+export const { Link, Navigate } = components<Path, Params>();
+export const { useModals, useNavigate, useParams } = hooks<Path, Params, ModalPath>();
+export const { redirect } = utils<Path, Params>();
