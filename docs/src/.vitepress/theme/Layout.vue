b0f8e8ef8b60d2c131ac70b3c20fff9e2b586cbf
zty012 <me@zty012.de>
2025-03-16 12:27:57 +0800

    :globe_with_meridians: 修改文档i18n结构

--- docs/src/.vitepress/theme/Layout.vue
+++ docs/src/.vitepress/theme/Layout.vue
@@ -6,9 +6,9 @@ import {
 import { NolebaseHighlightTargetedHeading } from "@nolebase/vitepress-plugin-highlight-targeted-heading";
 import { useData } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import { nextTick, provide } from "vue";
+import { nextTick, onMounted, provide } from "vue";
 
-const { isDark, localeIndex } = useData();
+const { isDark, localeIndex, site } = useData();
 
 const enableTransitions = () =>
   "startViewTransition" in document && window.matchMedia("(prefers-reduced-motion: no-preference)").matches;
@@ -38,6 +38,40 @@ provide("toggle-appearance", async ({ clientX: x, clientY: y }: MouseEvent) => {
     },
   );
 });
+
+const locales = Object.keys(site.value.locales);
+const path = window.location.pathname;
+const browserLanguage = navigator.language.toLowerCase();
+
+onMounted(() => {
+  let localeDefinedInPath = false;
+  for (const locale of locales) {
+    if (path.startsWith(`/${locale}`)) {
+      localeDefinedInPath = true;
+      break;
+    }
+  }
+  console.log(localeDefinedInPath, browserLanguage);
+  if (!localeDefinedInPath) {
+    if (browserLanguage.startsWith("en")) {
+      addLocalePrefixToPath("en");
+    }
+    if (browserLanguage.startsWith("zh")) {
+      addLocalePrefixToPath("zh_CN");
+    }
+  }
+});
+
+function addLocalePrefixToPath(locale: string) {
+  const newPath = `/${locale}/${path}`;
+  const el = document.createElement("a");
+  el.href = newPath;
+  el.classList.add("visually-hidden");
+  document.body.appendChild(el);
+  el.click();
+  el.blur();
+  el.remove();
+}
 </script>
 
 <template>
@@ -52,7 +86,7 @@ provide("toggle-appearance", async ({ clientX: x, clientY: y }: MouseEvent) => {
       <NolebaseHighlightTargetedHeading />
     </template>
     <template #doc-before>
-      <div class="warning custom-block" v-if="localeIndex !== 'zh'">
+      <div class="warning custom-block" v-if="localeIndex !== 'zh_CN'">
         <p class="custom-block-title">This document may be outdated!</p>
         <p>We don’t have enough time and energy to translate all the documents.</p>
       </div>
