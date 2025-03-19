882beeed3c859777b3497daf16b8b5b4266c0f83
littlefean <2028140990@qq.com>
2025-03-20 03:46:53 +0800

    🚸 可以选中多个图片，使用ctrl+滚轮键统一缩放大小了

--- app/src/core/service/controlService/controller/concrete/ControllerImageScale.tsx
+++ app/src/core/service/controlService/controller/concrete/ControllerImageScale.tsx
@@ -1,6 +1,7 @@
 import { Vector } from "../../../../dataStruct/Vector";
 import { Renderer } from "../../../../render/canvas2d/renderer";
 import { StageManager } from "../../../../stage/stageManager/StageManager";
+import { ImageNode } from "../../../../stage/stageObject/entity/ImageNode";
 import { Controller } from "../Controller";
 import { ControllerClass } from "../ControllerClass";
 
@@ -14,11 +15,13 @@ ControllerImageScale.mousewheel = (event: WheelEvent) => {
       return;
     }
     // 需要注意缩放逻辑和视野缩放逻辑保持一致性
-    if (event.deltaY > 0) {
-      // 放大图片
-      hoverImageNode.scaleUpdate(-0.1);
-    } else if (event.deltaY < 0) {
-      hoverImageNode.scaleUpdate(+0.1);
+    for (const imageNode of StageManager.getSelectedEntities().filter((entity) => entity instanceof ImageNode)) {
+      if (event.deltaY > 0) {
+        // 放大图片
+        imageNode.scaleUpdate(-0.1);
+      } else if (event.deltaY < 0) {
+        imageNode.scaleUpdate(+0.1);
+      }
     }
   }
 };
