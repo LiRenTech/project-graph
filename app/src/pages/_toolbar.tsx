6c577dbddefe82c98589140620d85f94ebaccb9b
littlefean <2028140990@qq.com>
2025-03-30 16:30:22 +0800

    💄 优化工具栏样式

--- app/src/pages/_toolbar.tsx
+++ app/src/pages/_toolbar.tsx
@@ -2,6 +2,7 @@ import { save as saveFileDialog } from "@tauri-apps/plugin-dialog";
 
 import {
   BrainCircuit,
+  Brush,
   ClipboardPaste,
   ClipboardX,
   Globe,
@@ -10,10 +11,10 @@ import {
   Package,
   PaintBucket,
   Palette,
-  PenTool,
   RefreshCcw,
   Repeat,
   SaveAll,
+  Slash,
   Square,
   Tag,
   Trash2,
@@ -44,9 +45,10 @@ interface ToolbarItemProps {
   icon: React.ReactNode; // 定义 icon 的类型
   handleFunction: () => void; // 定义 handleFunction 的类型
   description: string;
+  isHighlight?: boolean;
 }
 
-export function ToolbarItem({ icon, handleFunction, description }: ToolbarItemProps) {
+export function ToolbarItem({ icon, handleFunction, description, isHighlight = false }: ToolbarItemProps) {
   return (
     <div
       className="hover:bg-toolbar-icon-hover-bg text-toolbar-tooltip-text group relative flex h-8 w-8 cursor-pointer items-center justify-center rounded-md active:scale-90"
@@ -56,9 +58,12 @@ export function ToolbarItem({ icon, handleFunction, description }: ToolbarItemPr
       <span className="bg-toolbar-tooltip-bg border-toolbar-tooltip-border text-toolbar-tooltip-text pointer-events-none absolute bottom-8 z-10 w-auto origin-right scale-90 whitespace-nowrap rounded border p-1 text-xs opacity-0 group-hover:scale-100 group-hover:opacity-100">
         {description}
       </span>
+      {isHighlight && <div className="bg-panel-success-text absolute bottom-0 h-1 w-6 rounded-t-md" />}
     </div>
   );
 }
+
+const toolBarGroupStyle = "bg-toolbar-bg border-toolbar-border flex items-center rounded-md border";
 
 /**
  * 工具栏
@@ -71,6 +76,8 @@ export default function Toolbar({ className = "" }: { className?: string }) {
   const [isHaveSelectedNode, setSsHaveSelectedNode] = useState(false);
   const [isHaveSelectedEdge, setSsHaveSelectedEdge] = useState(false);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [isSelecting, setIsSelecting] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
 
   const update = () => {
     setSsHaveSelectedNode(StageManager.selectedNodeCount > 0);
@@ -94,131 +101,154 @@ export default function Toolbar({ className = "" }: { className?: string }) {
 
   // 以后适配安卓的时候再解决工具栏过小的问题
   return (
-    <Box
-      className={cn("bg-toolbar-bg border-toolbar-border fixed bottom-2 left-1/2 flex translate-x-[-50%]", className)}
-    >
-      {/* 右键移动画布的用户希望保留此按钮 */}
-      <ToolbarItem
-        description="删除选中的节点/连线"
-        icon={<Trash2 />}
-        handleFunction={() => {
-          StageManager.deleteSelectedStageObjects();
-        }}
-      />
-      <ToolbarItem
-        description="通过文本生成节点"
-        icon={<ClipboardPaste />}
-        handleFunction={() => Popup.show(<GenerateNodePanel />, true)}
-      />
-      {isHaveSelectedEdge && (
+    <Box className={cn("fixed bottom-2 left-1/2 flex translate-x-[-50%] gap-2 border-none", className)}>
+      <div className={toolBarGroupStyle}>
+        {/* 右键移动画布的用户希望保留此按钮 */}
         <ToolbarItem
-          description="反转选中连线方向"
-          icon={<Repeat />}
+          description="删除选中的节点/连线"
+          icon={<Trash2 />}
           handleFunction={() => {
-            const selectedEdges = StageManager.getLineEdges().filter((edge) => edge.isSelected);
-            StageNodeConnector.reverseEdges(selectedEdges);
-            StageHistoryManager.recordStep();
+            StageManager.deleteSelectedStageObjects();
           }}
         />
-      )}
+        <ToolbarItem
+          description="通过文本生成节点"
+          icon={<ClipboardPaste />}
+          handleFunction={() => Popup.show(<GenerateNodePanel />, true)}
+        />
+        {isHaveSelectedEdge && (
+          <ToolbarItem
+            description="反转选中连线方向"
+            icon={<Repeat />}
+            handleFunction={() => {
+              const selectedEdges = StageManager.getLineEdges().filter((edge) => edge.isSelected);
+              StageNodeConnector.reverseEdges(selectedEdges);
+              StageHistoryManager.recordStep();
+            }}
+          />
+        )}
 
-      <ToolbarItem
-        description="设置节点/连线/框的颜色，注意要先选中再点颜色 （F6）"
-        icon={<PaintBucket />}
-        handleFunction={() => Popup.show(<ColorPanel />, true)}
-      />
-      <ToolbarItem
-        description="设置实体创建时自动填充的颜色"
-        icon={<Palette />}
-        handleFunction={() => Panel.show({ title: "颜色自动填充" }, <ColorAutoPanel />)}
-      />
+        <ToolbarItem
+          description="设置节点/连线/框的颜色，注意要先选中再点颜色 （F6）"
+          icon={<PaintBucket />}
+          handleFunction={() => Popup.show(<ColorPanel />, true)}
+        />
+        <ToolbarItem
+          description="设置实体创建时自动填充的颜色"
+          icon={<Palette />}
+          handleFunction={() => Panel.show({ title: "颜色自动填充" }, <ColorAutoPanel />)}
+        />
 
-      <ToolbarItem
-        description="节点对齐相关"
-        icon={<LayoutDashboard />}
-        handleFunction={() => Popup.show(<AlignNodePanel />, true)}
-      />
-      {isClipboardClearShow && (
         <ToolbarItem
-          description="清空粘贴板内容"
-          icon={<ClipboardX />}
+          description="节点对齐相关"
+          icon={<LayoutDashboard />}
+          handleFunction={() => Popup.show(<AlignNodePanel />, true)}
+        />
+        {isClipboardClearShow && (
+          <ToolbarItem
+            description="清空粘贴板内容"
+            icon={<ClipboardX />}
+            handleFunction={() => {
+              CopyEngine.clearVirtualCopyBoardData();
+            }}
+          />
+        )}
+        {isHaveSelectedNode && (
+          <ToolbarItem
+            description="将选中的节点的内容作为网页链接或本地文件路径打开"
+            icon={<Globe />}
+            handleFunction={async () => {
+              // 打开文件或网页
+              openBrowserOrFile();
+            }}
+          />
+        )}
+        {isHaveSelectedNode && (
+          <ToolbarItem
+            description="将选中的节点保存为新文件"
+            icon={<SaveAll />}
+            handleFunction={() => {
+              onSaveSelectedNew();
+            }}
+          />
+        )}
+        <ToolbarItem
+          description="将选中节点打包Section（快捷键可自定义）"
+          icon={<Square />}
           handleFunction={() => {
-            CopyEngine.clearVirtualCopyBoardData();
+            StageManager.packEntityToSectionBySelected();
           }}
         />
-      )}
-      {isHaveSelectedNode && (
         <ToolbarItem
-          description="将选中的节点的内容作为网页链接或本地文件路径打开"
-          icon={<Globe />}
-          handleFunction={async () => {
-            // 打开文件或网页
-            openBrowserOrFile();
-          }}
-        />
-      )}
-      {isHaveSelectedNode && (
-        <ToolbarItem
-          description="将选中的节点保存为新文件"
-          icon={<SaveAll />}
+          description="切换Section的折叠状态（快捷键可自定义）（还在开发中，暂时不推荐使用）"
+          icon={<Package />}
           handleFunction={() => {
-            onSaveSelectedNew();
+            StageManager.sectionSwitchCollapse();
           }}
         />
-      )}
-      <ToolbarItem
-        description="将选中节点打包Section（快捷键可自定义）"
-        icon={<Square />}
-        handleFunction={() => {
-          StageManager.packEntityToSectionBySelected();
-        }}
-      />
-      <ToolbarItem
-        description="切换Section的折叠状态（快捷键可自定义）（还在开发中，暂时不推荐使用）"
-        icon={<Package />}
-        handleFunction={() => {
-          StageManager.sectionSwitchCollapse();
-        }}
-      />
-      {isHaveSelectedNode && (
+        {isHaveSelectedNode && (
+          <ToolbarItem
+            description="添加标签，如果已添加则去除标签"
+            icon={<Tag />}
+            handleFunction={() => {
+              StageManager.addTagBySelected();
+            }}
+          />
+        )}
+        {isHaveSelectedNode && (
+          <ToolbarItem
+            description="AI扩展节点，（已欠费，有待更新）"
+            icon={<BrainCircuit />}
+            handleFunction={() => {
+              StageGeneratorAI.generateNewTextNodeBySelected();
+              StageHistoryManager.recordStep();
+            }}
+          />
+        )}
         <ToolbarItem
-          description="添加标签，如果已添加则去除标签"
-          icon={<Tag />}
+          description="刷新选中内容(图片加载失败了可以选中图片然后点这个按钮)"
+          icon={<RefreshCcw />}
           handleFunction={() => {
-            StageManager.addTagBySelected();
+            StageManager.refreshSelected();
           }}
         />
-      )}
-      {isHaveSelectedNode && (
+      </div>
+
+      <div className={toolBarGroupStyle}>
         <ToolbarItem
-          description="AI扩展节点，（已欠费，有待更新）"
-          icon={<BrainCircuit />}
+          description="左键：框选和移动模式"
+          icon={<MousePointer />}
           handleFunction={() => {
-            StageGeneratorAI.generateNewTextNodeBySelected();
-            StageHistoryManager.recordStep();
-          }}
-        />
-      )}
-      <ToolbarItem
-        description="刷新选中内容(图片加载失败了可以选中图片然后点这个按钮)"
-        icon={<RefreshCcw />}
-        handleFunction={() => {
-          StageManager.refreshSelected();
-        }}
-      />
-      <ToolbarItem
-        description="切换涂鸦和鼠标"
-        icon={isDrawing ? <PenTool /> : <MousePointer />}
-        handleFunction={() => {
-          if (Stage.drawingMachine.isUsing) {
             Stage.drawingMachine.shutDown();
+            setIsSelecting(true);
             setIsDrawing(false);
-          } else {
+            setIsConnecting(false);
+          }}
+          isHighlight={isSelecting}
+        />
+        <ToolbarItem
+          description="左键：涂鸦模式"
+          icon={<Brush className="rotate-90" />}
+          handleFunction={() => {
             Stage.drawingMachine.open();
+            setIsSelecting(false);
             setIsDrawing(true);
-          }
-        }}
-      />
+            setIsConnecting(false);
+          }}
+          isHighlight={isDrawing}
+        />
+        <ToolbarItem
+          description="左键：连接与断开（正在开发中，先用右键）"
+          icon={<Slash className="rotate-90" />}
+          handleFunction={() => {
+            Stage.drawingMachine.open();
+            setIsSelecting(false);
+            setIsDrawing(false);
+            setIsConnecting(true);
+          }}
+          isHighlight={isConnecting}
+        />
+      </div>
     </Box>
   );
 }
