37d5440cdef64dfc9e2ddc3c6db8a875a7801965
zty012 <me@zty012.de>
2025-04-03 20:18:56 +0800

    🐛 修复构建脚本

--- app/scripts/tauri_build.js
+++ app/scripts/tauri_build.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import { spawn } from "child_process";
-import path from "path";
 
 // 从环境变量获取参数并分割成数组
 const tauriBuildArgs = process.env.TAURI_BUILD_ARGS ? process.env.TAURI_BUILD_ARGS.split(" ") : [];
@@ -8,10 +7,10 @@ const tauriBuildArgs = process.env.TAURI_BUILD_ARGS ? process.env.TAURI_BUILD_AR
 // 构造完整命令参数
 const args = ["tauri", "build", ...tauriBuildArgs];
 
-const pnpmBin = path.join(process.cwd(), "node_modules/.bin/pnpm");
+const pnpmBin = process.env.npm_execpath;
 
 // 使用 spawn 执行命令
-const child = spawn(pnpmBin, args, {
+const child = spawn(pnpmBin.endsWith("js") ? `node ${pnpmBin}` : pnpmBin, args, {
   stdio: "inherit", // 继承 stdio 以便实时输出
 });
 
