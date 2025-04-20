7e69bb825912e43e2419ac2296f91668a0770de3
littlefean <2028140990@qq.com>
2025-04-20 10:57:44 +0800

    🐛 修复右上角X在白色主题下看不到的问题

--- app/src/pages/_floating_outlet.tsx
+++ app/src/pages/_floating_outlet.tsx
@@ -11,7 +11,7 @@ export default function FloatingOutlet() {
       <div className="bg-settings-page-bg shadow-settings-page-bg h-8/9 w-7/8 fixed left-1/2 top-1/2 z-[70] -translate-x-1/2 -translate-y-1/2 rounded-xl shadow-2xl lg:w-4/5">
         <Outlet />
         <X
-          className="text-button-text absolute right-4 top-4 cursor-pointer hover:rotate-90"
+          className="text-panel-text absolute right-4 top-4 cursor-pointer hover:rotate-90"
           id="close-popin-btn"
           onClick={() => {
             console.log("closing popin");
