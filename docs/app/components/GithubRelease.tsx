2f0a9832cce0148495106b25b6fdc55cd99d3716
littlefean <2028140990@qq.com>
2025-04-20 22:30:38 +0800

    📝 更新文档：macOs版本损坏无法打开的提示

--- docs/app/components/GithubRelease.tsx
+++ docs/app/components/GithubRelease.tsx
@@ -65,15 +65,7 @@ export default function GithubRelease() {
         </Tab>
         <Tab value="macOS (Apple Silicon)">
           <Callout type="warn">此处的 macOS 版本由Github Actions自动构建，可能无法正常运行。</Callout>
-          <Callout type="info">
-            解决方法：可尝试在网盘下载开发者手动构建的 Mac Apple Silicon版本。 —— 2025年4月15日
-          </Callout>
-          <Callout type="info">
-            网盘链接在下方。若网盘版本也无法运行，或联系开发者反馈系统版本和M芯片型号帮助我们排查问题。 —— 2025年4月19日
-          </Callout>
-          <Callout type="info">
-            若您有兴趣参与项目的开发，可尝试安装node、pnpm、rust等环境自行编译，详见 开发指南
-          </Callout>
+          <Callout type="info">解决方法详见：https://project-graph.top/docs/app/faq/mac</Callout>
           <FindAsset release={release} endwith="aarch64.dmg" />
           <FindAsset release={nightlyRelease} endwith="aarch64.dmg" />
         </Tab>
