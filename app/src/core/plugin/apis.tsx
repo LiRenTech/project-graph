0d31ced5ecdf9fcfb383ec72fb5b6a8dd8d7a740
littlefean <2028140990@qq.com>
2025-04-20 17:42:00 +0800

    👽️ hello函数更新API

--- app/src/core/plugin/apis.tsx
+++ app/src/core/plugin/apis.tsx
@@ -5,8 +5,13 @@ import { Camera } from "../stage/Camera";
 import { PluginAPI } from "./types";
 
 export const pluginApis: PluginAPI = {
-  hello() {
-    return "Hello from Project Graph";
+  hello(userString: string) {
+    console.log("用户插件调用hello", userString);
+    Dialog.show({
+      title: "插件调用成功",
+      content: `你好，${userString}`,
+      type: "success",
+    });
   },
   getCameraLocation() {
     return Camera.location.toArray();
