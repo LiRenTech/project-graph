8d2bc58d6682f4d714e146a01e74358607888a54
littlefean <2028140990@qq.com>
2025-04-29 19:02:55 +0800

    🚧 添加直线连线被斩断的特效

---
+++ app/src/core/service/feedbackService/effectEngine/concrete/EdgeCutEffect.tsx
@@ -0,0 +1,12 @@
+import { EffectObject } from "../effectObject";
+
+/**
+ * 直线连线被斩断的特效
+ */
+export class EdgeCutEffect extends EffectObject {
+  getClassName(): string {
+    return "EdgeCutEffect";
+  }
+
+  render() {}
+}
