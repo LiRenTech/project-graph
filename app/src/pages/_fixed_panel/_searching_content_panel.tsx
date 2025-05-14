656a3855db9af51120fd9432528d8bc318f2d102
littlefean <2028140990@qq.com>
2025-05-14 15:55:58 +0800

    🐛 修复搜索内容面板按钮溢出问题

--- app/src/pages/_fixed_panel/_searching_content_panel.tsx
+++ app/src/pages/_fixed_panel/_searching_content_panel.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { cn } from "../../utils/cn";
 import IconButton from "../../components/IconButton";
 import { Stage } from "../../core/stage/Stage";
-import { CaseSensitive, CaseUpper, SquareDashedMousePointer } from "lucide-react";
+import { CaseSensitive, CaseUpper, Delete, SquareDashedMousePointer } from "lucide-react";
 import Input from "../../components/Input";
 import { StageManager } from "../../core/stage/stageManager/StageManager";
 import { Camera } from "../../core/stage/Camera";
@@ -44,46 +44,14 @@ export default function SearchingContentPanel({ open = false, className = "" }: 
   return (
     <div
       className={cn(
-        "bg-panel-bg text-panel-text fixed -left-72 top-16 flex h-full w-72 flex-col rounded-md p-4 pb-20 transition-all",
+        "bg-panel-bg text-panel-text fixed -left-72 top-16 flex h-full w-72 flex-col rounded-md p-2 pb-20 transition-all",
         {
           "left-0": open,
         },
         className,
       )}
     >
-      {searchResults.length > 0 && (
-        <IconButton onClick={selectAllResult} tooltip={"将全部结果选中"}>
-          <SquareDashedMousePointer />
-        </IconButton>
-      )}
-      <div className="flex items-center justify-between">
-        <Input
-          placeholder="请输入要搜索的内容"
-          className="search-panel-input"
-          onChange={(value) => {
-            setSearchString(value);
-            Stage.contentSearchEngine.startSearch(value, false);
-            setSearchResults(
-              Stage.contentSearchEngine.searchResultNodes.map((node) => ({
-                title: Stage.contentSearchEngine.getStageObjectText(node),
-                uuid: node.uuid,
-              })),
-            );
-          }}
-          onKeyDown={(e) => {
-            if (e.ctrlKey || e.altKey || e.metaKey) {
-              // 取消默认事件
-              e.preventDefault();
-            } else if (e.key === "Escape") {
-              if (searchString !== "") {
-                // 取消搜索
-                setSearchString("");
-                setSearchResults([]);
-              }
-            }
-          }}
-          value={searchString}
-        />
+      <div className="my-1 flex justify-between">
         <IconButton
           onClick={() => {
             const currentResult = !isCaseSensitive;
@@ -94,7 +62,49 @@ export default function SearchingContentPanel({ open = false, className = "" }: 
         >
           {isCaseSensitive ? <CaseSensitive /> : <CaseUpper />}
         </IconButton>
+        <IconButton onClick={selectAllResult} tooltip={"将全部结果选中"} disabled={searchResults.length === 0}>
+          <SquareDashedMousePointer />
+        </IconButton>
+
+        <IconButton
+          onClick={() => {
+            setSearchString("");
+            Stage.contentSearchEngine.startSearch("", false);
+            setSearchResults([]);
+          }}
+          tooltip={"删除输入内容与结果"}
+        >
+          <Delete />
+        </IconButton>
       </div>
+
+      <Input
+        placeholder="请输入要在舞台上搜索的内容"
+        className="search-panel-input"
+        onChange={(value) => {
+          setSearchString(value);
+          Stage.contentSearchEngine.startSearch(value, false);
+          setSearchResults(
+            Stage.contentSearchEngine.searchResultNodes.map((node) => ({
+              title: Stage.contentSearchEngine.getStageObjectText(node),
+              uuid: node.uuid,
+            })),
+          );
+        }}
+        onKeyDown={(e) => {
+          if (e.ctrlKey || e.altKey || e.metaKey) {
+            // 取消默认事件
+            e.preventDefault();
+          } else if (e.key === "Escape") {
+            if (searchString !== "") {
+              // 取消搜索
+              setSearchString("");
+              setSearchResults([]);
+            }
+          }
+        }}
+        value={searchString}
+      />
       <div className="overflow-y-auto">
         {searchResults.map((result, index) => (
           <div
