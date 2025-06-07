95bcd1fc02310d4da54fd6cc84850cb92f153b31
zty012 <me@zty012.de>
2025-06-07 17:10:47 +0800

    📝 修改文档主题

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,16 +1,11 @@
 import { RootProvider } from "fumadocs-ui/provider";
-import { Inter } from "next/font/google";
+import "katex/dist/katex.css";
 import type { ReactNode } from "react";
 import "./global.css";
-import "katex/dist/katex.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-});
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className="flex min-h-screen flex-col">
         <RootProvider
           search={{
---
+++ docs/lib/getLLMText.ts
@@ -0,0 +1,28 @@
+import { source } from "@/lib/source";
+import type { InferPageType } from "fumadocs-core/source";
+import { remarkInclude } from "fumadocs-mdx/config";
+import { remark } from "remark";
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import remarkMdx from "remark-mdx";
+
+const processor = remark()
+  .use(remarkMdx)
+  // needed for Fumadocs MDX
+  .use(remarkInclude)
+  .use(remarkGfm)
+  .use(remarkMath);
+
+export async function getLLMText(page: InferPageType<typeof source>) {
+  const processed = await processor.process({
+    path: page.data._file.absolutePath,
+    value: page.data.content,
+  });
+
+  return `# ${page.data.title}
+URL: ${page.url}
+
+${page.data.description}
+
+${processed.value}`;
+}
