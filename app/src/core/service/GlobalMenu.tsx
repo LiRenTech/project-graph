236e593457130c46f00ce4760510a77778d3945d
littlefean <2028140990@qq.com>
2025-07-15 00:00:56 +0800

    ✨ feat(core): 添加新的菜单项和图标

--- app/src/core/service/GlobalMenu.tsx
+++ app/src/core/service/GlobalMenu.tsx
@@ -1,6 +1,42 @@
 import { deserialize, serialize } from "@graphif/serializer";
 import { open } from "@tauri-apps/plugin-dialog";
-import { Bot, ExternalLink, File, FileDown, FilePlus, FolderOpen, Save, SettingsIcon, TestTube2 } from "lucide-react";
+import {
+  Airplay,
+  AppWindow,
+  Axe,
+  Bot,
+  CircleAlert,
+  Earth,
+  ExternalLink,
+  File,
+  FileCode,
+  FileDown,
+  FileImage,
+  FilePlus,
+  FileType,
+  Folder,
+  FolderClock,
+  FolderCog,
+  FolderOpen,
+  FolderTree,
+  Fullscreen,
+  Keyboard,
+  MapPin,
+  MessageCircleWarning,
+  PersonStanding,
+  Radiation,
+  Redo,
+  RefreshCcwDot,
+  Save,
+  Scaling,
+  SettingsIcon,
+  SquareDashedMousePointer,
+  TestTube2,
+  TextQuote,
+  Undo,
+  VenetianMask,
+  View,
+} from "lucide-react";
 import { ReactNode } from "react";
 import { URI } from "vscode-uri";
 import { Dialog } from "../../components/dialog";
@@ -68,7 +104,33 @@ export namespace GlobalMenu {
         project.save();
       }),
       new MenuItem("另存为", <FileDown />),
+      new Separator(),
+      new MenuItem("根据文件夹生成嵌套图", <FolderTree />, () => {}),
+      new Separator(),
+      new MenuItem("导出 SVG矢量图", <FileCode />, () => {}),
+      new MenuItem("导出 PNG图片", <FileImage />, () => {}),
+      new MenuItem("导出 Markdown格式", <FileType />, () => {}),
+      new MenuItem("导出 纯文本格式", <TextQuote />, () => {}),
     ]),
+    new Menu("位置", <Folder />, [
+      new MenuItem("打开软件配置文件夹", <FolderCog />, () => {}),
+      new MenuItem("打开软件缓存文件夹", <FolderClock />, () => {}),
+      new MenuItem("打开当前项目文件夹", <FolderOpen />, () => {}),
+    ]),
+    new Menu("视野", <View />, [
+      new MenuItem("根据全部内容重制视野", <Fullscreen />, () => {}),
+      new MenuItem("根据选中内容重制视野", <SquareDashedMousePointer />, () => {}),
+      new MenuItem("仅重制视野缩放到标准大小", <Scaling />, () => {}),
+      new MenuItem("移动视野到坐标轴原点", <MapPin />, () => {}),
+    ]),
+    new Menu("操作", <Axe />, [
+      new MenuItem("刷新", <RefreshCcwDot />, () => {}),
+      new MenuItem("撤销", <Undo />, () => {}),
+      new MenuItem("重做", <Redo />, () => {}),
+      new MenuItem("松开按键", <Keyboard />, () => {}),
+      new MenuItem("清空舞台", <Radiation />, () => {}),
+    ]),
+
     new Menu("设置", <SettingsIcon />, [
       new MenuItem("设置", <SettingsIcon />, () => {
         SettingsWindow.open("visual");
@@ -88,6 +150,18 @@ export namespace GlobalMenu {
       new MenuItem("打开 AI 面板", <ExternalLink />, () => {
         AIWindow.open();
       }),
+    ]),
+    new Menu("视图", <AppWindow />, [
+      new MenuItem("全屏", <Fullscreen />, () => {}),
+      new MenuItem("专注模式", <Airplay />, () => {}),
+      new MenuItem("隐私模式", <VenetianMask />, () => {}),
+    ]),
+    new Menu("关于", <CircleAlert />, [
+      new MenuItem("关于", <MessageCircleWarning />, () => {
+        AIWindow.open();
+      }),
+      new MenuItem("新手引导", <PersonStanding />, () => {}),
+      new MenuItem("进入官网", <Earth />, () => {}),
     ]),
   ];
 }
