dcc215957efa5fe2411b5ca3ff5858543f5c9ae4
zty012 <me@zty012.de>
2025-08-09 21:32:41 +0800

    🐛 修复连线时无法渲染virtual edge

--- app/src/core/service/controlService/MouseLocation.tsx
+++ app/src/core/service/controlService/MouseLocation.tsx
@@ -4,30 +4,36 @@ export const MouseLocation = {
   x: 0,
   y: 0,
   init() {
-    window.addEventListener("mousemove", (event) => {
+    window.addEventListener("pointerdown", (e) => {
+      if (e.button === 2) {
+        e.preventDefault();
+      }
+    });
+
+    window.addEventListener("pointermove", (event) => {
       this.x = event.clientX;
       this.y = event.clientY;
 
       // this.project.controller.recordManipulate();
       // 检测是否超出范围
-      // TODO: 这里还可以优化一下，给每个Controller都加一个mouseMoveOutWindowForcedShutdown方法
+      // TODO: 优化，给每个 Controller 加一个 pointerMoveOutWindowForcedShutdown 方法
       // if (this.x < 0 || this.x > window.innerWidth || this.y < 0 || this.y > window.innerHeight) {
       //   if (this.project.controller.cutting.isUsing) {
-      //     this.project.controller.cutting.mouseMoveOutWindowForcedShutdown(this.vectorObject);
+      //     this.project.controller.cutting.pointerMoveOutWindowForcedShutdown(this.vectorObject);
       //   }
       //   if (this.project.controller.camera.isUsingMouseGrabMove) {
-      //     this.project.controller.camera.mouseMoveOutWindowForcedShutdown(this.vectorObject);
+      //     this.project.controller.camera.pointerMoveOutWindowForcedShutdown(this.vectorObject);
       //   }
       //   if (this.project.controller.rectangleSelect.isUsing) {
-      //     this.project.controller.rectangleSelect.mouseMoveOutWindowForcedShutdown(this.vectorObject);
+      //     this.project.controller.rectangleSelect.pointerMoveOutWindowForcedShutdown(this.vectorObject);
       //   }
-      //   this.project.controller.entityClickSelectAndMove.mouseMoveOutWindowForcedShutdown(this.vectorObject);
+      //   this.project.controller.entityClickSelectAndMove.pointerMoveOutWindowForcedShutdown(this.vectorObject);
       // }
     });
   },
   /**
-   * 返回的时视野坐标系中的鼠标位置
-   * 注意是view坐标系
+   * 返回视野坐标系中的鼠标位置
+   * 注意：此处返回的是 view 坐标系中的位置
    * @returns
    */
   vector(): Vector {
